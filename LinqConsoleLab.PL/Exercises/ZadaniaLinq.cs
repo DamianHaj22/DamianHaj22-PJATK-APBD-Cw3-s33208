24d221fae7cabc89ab2976c31de05e8d119fe4da
Damian Hajduk <s33208@pjwstk.edu.pl>
2026-04-02 18:12:45 +0200

    Zadanie 3

--- LinqConsoleLab.PL/Exercises/ZadaniaLinq.cs
+++ LinqConsoleLab.PL/Exercises/ZadaniaLinq.cs
@@ -47,7 +47,11 @@ public sealed class ZadaniaLinq
     /// </summary>
     public IEnumerable<string> Zadanie03_StudenciPosortowani()
     {
-        throw Niezaimplementowano(nameof(Zadanie03_StudenciPosortowani));
+        return DaneUczelni.Studenci
+            .OrderBy(student => student.Nazwisko)
+            .ThenBy(student => student.Imie)
+            .Select(student => $"{student.NumerIndeksu} {student.Imie} {student.Nazwisko}");
+
     }
 
     /// <summary>
