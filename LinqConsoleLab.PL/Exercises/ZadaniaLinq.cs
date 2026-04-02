ec2c63b8953186c0439be31780fbd0dd36cfff26
Damian Hajduk <s33208@pjwstk.edu.pl>
2026-04-02 18:36:33 +0200

    Zadanie 6

--- LinqConsoleLab.PL/Exercises/ZadaniaLinq.cs
+++ LinqConsoleLab.PL/Exercises/ZadaniaLinq.cs
@@ -106,7 +106,8 @@ public sealed class ZadaniaLinq
     /// </summary>
     public IEnumerable<string> Zadanie06_CzyWszyscyProwadzacyMajaKatedre()
     {
-        throw Niezaimplementowano(nameof(Zadanie06_CzyWszyscyProwadzacyMajaKatedre));
+        bool czyWszyscyMaja = DaneUczelni.Prowadzacy.All(p => !string.IsNullOrWhiteSpace(p.Katedra));
+        return [$"Czy każdy prowadzący ma uzupełnioną nazwę katedry? {czyWszyscyMaja}"];
     }
 
     /// <summary>
--- LinqConsoleLab.PL/obj/Debug/net9.0/LinqConsoleLab.PL.AssemblyInfo.cs
+++ LinqConsoleLab.PL/obj/Debug/net9.0/LinqConsoleLab.PL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LinqConsoleLab.PL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+196e220ca0cc43b5ccdf980bc05126742dee4145")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81d4106524c55cbdd6a0a1e771c701b46712cf44")]
 [assembly: System.Reflection.AssemblyProductAttribute("LinqConsoleLab.PL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LinqConsoleLab.PL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
